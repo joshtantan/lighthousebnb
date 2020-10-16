474025a9825ec9572bff954632ad6abdf901020e
Joshua Tan <josh.tan.tann@gmail.com>
2020-10-15 22:15:03 -0700

    Update addProperties
    Signed-off-by: Joshua Tan <josh.tan.tann@gmail.com>

--- 1_queries/03_property_listings_by_city.sql
+++ 1_queries/03_property_listings_by_city.sql
@@ -1,8 +1,8 @@
-SELECT properties.*, avg(property_reviews.rating) as average_rating
+SELECT properties.*, AVG(property_reviews.rating) AS average_rating
 FROM properties
 JOIN property_reviews ON properties.id = property_id
 WHERE city LIKE '%ancouv%'
 GROUP BY properties.id
-HAVING avg(property_reviews.rating) >= 4
+HAVING AVG(property_reviews.rating) >= 4
 ORDER BY cost_per_night
 LIMIT 10;
--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -192,10 +192,22 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryParams = [];
+
+  for (const key in property) {
+    queryParams.push(property[key]);
+  }
+
+  return pool.query(`
+    INSERT INTO properties (title, description, number_of_bedrooms, number_of_bathrooms, parking_spaces, cost_per_night, thumbnail_photo_url, cover_photo_url, street, country, city, province, post_code, owner_id)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+    RETURNING *;
+    `, queryParams)
+    .then(res => res.rows[0])
+    .catch(e => {
+      console.error(e);
+      res.send(e);
+    });
 }
 
 exports.addProperty = addProperty;
